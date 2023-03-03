cc3b7a4bc118a5a84941aa76b5f9b203d36c89ef
Petar Penev <peteripenev@gmail.com>
2023-03-03 12:30:12 -0800

    Finish building tests

--- scripts/manipulateSILVAtrees.py
+++ scripts/manipulateSILVAtrees.py
@@ -18,12 +18,15 @@ def create_and_parse_argument_options(argument_list):
     parser.add_argument('nucleotideIndex', nargs=2, help='Takes 2 integers, both indexing the nucleotide columns of interest in the STOCKHOLM file.')
     parser.add_argument('-t','--taxonomyName', help='Taxonomy name to filter the SILVA tree by (include it and all of its children: e.g. Bacteria)\n\
     Should be available in the map file.', default=None)
+    parser.add_argument('-ct','--save_temp_anno_tree', help='Save an iTOL annotation for the tree with color coding for temperature optimum values.', action='store_true')
     commandline_args = parser.parse_args(argument_list)
     return commandline_args
 
 def map_names_from_taxmap(mapFile):
+    '''Reads the SILVA taxmap file and returns a dictionary with SILVA IDs as keys 
+    and a list of tuples with (organism_name, primaryAccession, path) as values.'''
     nameMapper = dict()
-    with open(mapFile) as mapFile:                                                                                          
+    with open(mapFile) as mapFile:
         reader = csv.DictReader(mapFile, delimiter='\t')
         #next(reader)
         for entry in reader:
@@ -33,14 +36,22 @@ def map_names_from_taxmap(mapFile):
     return nameMapper
 
 def read_taxonomy_map(treeFile):
+    '''Reads the SILVA taxonomy map file and returns a dictionary with SILVA IDs as keys
+    Uses as input the .tre file, and assumes the .map file is in the same directory with the same name.'''
     taxonomyMapper = dict()
-    with open(treeFile.replace('.tre','.map')) as mapFile:                                                                                          
+    with open(treeFile.replace('.tre','.map')) as mapFile:
         reader = csv.reader(mapFile, delimiter='\t')
         for entry in reader:
             taxonomyMapper[entry[0]] = entry[1]
     return taxonomyMapper
 
 def connect_accessions_with_nucl(tree, align, nuclOne, nuclTwo, nameMapper, taxonomyMapper):
+    '''Takes a tree, an alignment, two alignment positions, 
+    and a dictionary with SILVA IDs as keys and a list of tuples 
+    with (organism_name, primaryAccession, path) as values.
+    Returns a dictionary with accessions as keys and a list of nucleotide sequences as values.
+    Modifies the tree by adding a feature to each node with the nucleotide sequence of interest 
+    and removes nodes that don't have a match in the alignment.'''
     accessionToNucl = dict()
     for node in tree.traverse("postorder"):
         if node.name in nameMapper.keys():
@@ -68,6 +79,8 @@ def connect_accessions_with_nucl(tree, align, nuclOne, nuclTwo, nameMapper, taxo
     return accessionToNucl
 
 def remove_nodes_from_tree(truncatedTree, namesForTruncation):
+    '''Takes a tree and a list of names to remove from the tree.
+    Returns a new tree with the nodes removed and a list of the removed nodes.'''
     removed_Nodes = list()
     for node in truncatedTree.get_leaves():
         if node.name not in namesForTruncation:
@@ -75,6 +88,74 @@ def remove_nodes_from_tree(truncatedTree, namesForTruncation):
         removedNode = node.detach()
         removed_Nodes.append(removedNode)
     return truncatedTree, removed_Nodes
+
+def combine_tempura_with_tree(tempuraFileLoc, truncatedTree):
+    '''Takes a TEMPURA file and a tree.
+    Returns a list of all temperature optimum values, 
+    a list of temperature optimum values that match the tree, 
+    and the leaves of the tree that have a match in the TEMPURA file.'''
+    toptMatchList, toptList, tempLabeledLeaves = list(), list(), list()
+    with open(tempuraFileLoc) as tempuraFile:
+        next(tempuraFile)
+        reader = csv.reader(tempuraFile, delimiter=',')
+        for entry in reader:
+            toptList.append(float(entry[15]))
+            matchingNames = list(filter(lambda n: n.name.replace('Candidatus ','') in entry[0], truncatedTree.traverse() ))
+            if len(matchingNames) > 0:
+                matchingNames[0].add_features(topt_ave=float(entry[15]))
+                tempLabeledLeaves.append(matchingNames[0].name)
+                toptMatchList.append(float(entry[15]))
+    return toptList, toptMatchList, tempLabeledLeaves
+
+def save_colored_tree_by_tempura(taxonomyName, truncatedTree, toptList, mapper, location='./temperatureTrees/', labelTextBaselineTemp=50):
+    '''Takes a tree, a list of temperature optimum values, and a matplotlib color mapper.
+    Saves a file with annotations for iTOL to color the tree by temperature optimum values.'''
+    with open(f'{location}temperatureLabelColors_{taxonomyName}.txt', 'w') as f:
+        f.write(f'DATASET_STYLE\r\nSEPARATOR COMMA\r\nDATASET_LABEL,Colors {min(toptList)}-{max(toptList)}\r\nCOLOR,#ffff00\r\nDATA\r\n')
+        for node in truncatedTree.get_leaves():
+            labelColor = '#ffffff'
+            labelTextColor = '#000000'
+            if 'topt_ave' in node.features:
+                labelColor = rgb2hex(mapper.to_rgba(node.topt_ave))
+                if node.topt_ave < labelTextBaselineTemp:
+                    labelTextColor = '#ffffff'
+            f.write(f'{node.name.replace("Candidatus ", "Cand. ")},label,node,{labelTextColor},1,normal,{labelColor}\r\n')
+
+def generate_nucleotide_type_set(accessionToNucl):
+    '''Takes a dictionary with accessions as keys and a list of nucleotide sequences as values.
+    Returns a list of nucleotide types ordered by count and a list of nucleotide types without duplicates.'''
+    downsetted = [list(set(x)) for x in accessionToNucl.values()]
+    only_single_entries = [list(x)[0] for x in downsetted if len(x) == 1]
+    orderedNuclTypesByCount = sorted(only_single_entries, key=only_single_entries.count,reverse=True)
+    setOfNuclTypes = list(dict.fromkeys(orderedNuclTypesByCount))
+    setOfNuclTypes.append('XX')
+    return setOfNuclTypes
+
+def save_shape_annotations(shapes, setOfNuclTypes, accessionToNucl, taxonomyName, truncatedTree, outpath='./temperatureTrees/'):
+    '''Takes a list of iTOL shapes, a set of nucleotide types, a dictionary with accessions as keys and a list of nucleotide sequences as values,
+    a taxonomy name, a tree, and an output path.
+    Saves a file with annotations for iTOL to color the tree by temperature optimum values.'''
+    nuclShapes = [shapes[x] if len(shapes) > x else "GP" for x in range(len(setOfNuclTypes))]
+    nuclTypesToShapes = {setOfNuclTypes[i]: nuclShapes[i] for i in range(len(setOfNuclTypes))}
+    stringOfNuclTypes = ','.join(nuclShapes)
+    stringOfNuclColors = ','.join(['#000000' for x in range(1,len(setOfNuclTypes)+1)])
+    multiMatches = [(k,v) for k,v in accessionToNucl.items() if len(v) > 1]
+    with open(f'{outpath}interestNuclShapes_{taxonomyName}.txt', 'w') as f:
+        size = '15'
+        f.write('DATASET_SYMBOL\r\nSEPARATOR COMMA\r\nDATASET_LABEL,SILVA tree shapes\r\nCOLOR,#ff0000\r\n')
+        f.write(f'LEGEND_TITLE,Nucleotide types,\r\nLEGEND_SHAPES,{stringOfNuclTypes}\r\nLEGEND_COLORS,{stringOfNuclColors}\
+            \r\nLEGEND_LABELS,{",".join(setOfNuclTypes)}\r\nDATA\r\n')
+        for node in truncatedTree.get_leaves():
+            if 'nuclOfInterest' not in node.features:
+                continue
+            if len(set(node.nuclOfInterest)) > 1:
+                shape = nuclTypesToShapes['XX']
+            elif node.nuclOfInterest[0] in setOfNuclTypes:
+                shape = nuclTypesToShapes[node.nuclOfInterest[0]]
+            else:
+                shape = nuclTypesToShapes['XX']
+            f.write(f'{node.name.replace("Candidatus ", "Cand. ")},{shape},{size},#000000,1,1\r\n')
+    return truncatedTree
 
 def main(commandline_args):
     comm_args = create_and_parse_argument_options(commandline_args)
@@ -97,68 +178,19 @@ def main(commandline_args):
     namesForTruncation = ['uncultured', 'Rice Cluster I', 'J07HB67', 'J07HR59', 'J07HX64']
     truncatedTree, removed_Nodes = remove_nodes_from_tree(truncatedTree, namesForTruncation)
 
-    toptMatchList, toptList, tempLabeledLeaves = list(), list(), list()
-    with open(comm_args.tempuraFile) as tempuraFile:
-        next(tempuraFile)
-        reader = csv.reader(tempuraFile, delimiter=',')
-        for entry in reader:
-            toptList.append(float(entry[15]))
-            matchingNames = list(filter(lambda n: n.name.replace('Candidatus ','') in entry[0], truncatedTree.traverse() ))
-            if len(matchingNames) > 0:
-                matchingNames[0].add_features(topt_ave=float(entry[15]))
-                tempLabeledLeaves.append(matchingNames[0].name)
-                toptMatchList.append(float(entry[15]))
+    toptList, toptMatchList, tempLabeledLeaves = combine_tempura_with_tree(comm_args.tempuraFile, truncatedTree)
 
     #Hardcode the min to push 25 degrees to be green
     norm = Normalize(vmin=min(toptList), vmax=max(toptList), clip=True)
     mapper = cm.ScalarMappable(norm=norm, cmap=cm.viridis)
 
-    #with open(f'./temperatureTrees/temperatureLabelColors_{comm_args.taxonomyName}.txt', 'w') as f:
-    #   f.write(f'DATASET_STYLE\r\nSEPARATOR COMMA\r\nDATASET_LABEL,Colors {min(toptList)}-{max(toptList)}\r\nCOLOR,#ffff00\r\nDATA\r\n')
-    #   for node in truncatedTree.get_leaves():
-    #       labelColor = '#ffffff'
-    #       labelTextColor = '#000000'
-    #       if 'topt_ave' in node.features:
-    #           labelColor = rgb2hex(mapper.to_rgba(node.topt_ave))
-    #           if node.topt_ave < 50:
-    #               labelTextColor = '#ffffff'
-    #       f.write(f'{node.name.replace("Candidatus ", "Cand. ")},label,node,{labelTextColor},1,normal,{labelColor}\r\n')
+    if comm_args.save_temp_anno_tree:
+        save_colored_tree_by_tempura(comm_args.taxonomyName, truncatedTree, toptList, mapper)
     
-    downsetted = [list(set(x)) for x in accessionToNucl.values()]
-    #setOfNuclTypes = sorted(list(set([list(x)[0] for x in downsetted if len(x) == 1])),reverse=True)
-    only_single_entries = [list(x)[0] for x in downsetted if len(x) == 1]
-    orderedNuclTypesByCount = sorted(only_single_entries, key=only_single_entries.count,reverse=True)
-    setOfNuclTypes = list(dict.fromkeys(orderedNuclTypesByCount))
-    setOfNuclTypes.append('XX')
-    #Hardcoding for now; for later use a % of the unique nucleotide types
-    #len([x[0] for x in downsetted if x[0] == 'UU'])/len(downsetted)
-    #setOfNuclTypes = ['UU', 'CU', 'CC', 'UA', 'UC', 'XX']
+    setOfNuclTypes = generate_nucleotide_type_set(accessionToNucl)
     shapes = ["1","2","3","4","5","6","HH","HV","EL","DI","PL","PR","PU","PD","OC","GP"]
-    nuclShapes = [shapes[x] if len(shapes) > x else "GP" for x in range(len(setOfNuclTypes))]
-    nuclTypesToShapes = {setOfNuclTypes[i]: nuclShapes[i] for i in range(len(setOfNuclTypes))}
-    stringOfNuclTypes = ','.join(nuclShapes)
-    stringOfNuclColors = ','.join(['#000000' for x in range(1,len(setOfNuclTypes)+1)])
-    multiMatches = [(k,v) for k,v in accessionToNucl.items() if len(v) > 1]
-    with open(f'./temperatureTrees/bridge_interestNuclShapes_{comm_args.taxonomyName}.txt', 'w') as f:
-        size = '15'
-        f.write('DATASET_SYMBOL\r\nSEPARATOR COMMA\r\nDATASET_LABEL,SILVA tree shapes\r\nCOLOR,#ff0000\r\n')
-        f.write(f'LEGEND_TITLE,Nucleotide types,\r\nLEGEND_SHAPES,{stringOfNuclTypes}\r\nLEGEND_COLORS,{stringOfNuclColors}\
-            \r\nLEGEND_LABELS,{",".join(setOfNuclTypes)}\r\nDATA\r\n')
-        for node in truncatedTree.get_leaves():
-            labelColor = '#000000'
-            if 'nuclOfInterest' in node.features:
-                if len(set(node.nuclOfInterest)) > 1:
-                    shape = nuclTypesToShapes['XX']
-                elif node.nuclOfInterest[0] in setOfNuclTypes:
-                    shape = nuclTypesToShapes[node.nuclOfInterest[0]]
-                else:
-                    shape = nuclTypesToShapes['XX']
-            f.write(f'{node.name.replace("Candidatus ", "Cand. ")},{shape},{size},#000000,1,1\r\n')
-
+    truncatedTree = save_shape_annotations(shapes, setOfNuclTypes, accessionToNucl, comm_args.taxonomyName, truncatedTree)
     truncatedTree.write(format=1, outfile=f'./temperatureTrees/bridge_truncatedTree_{comm_args.taxonomyName}.nwk')
-
-
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- tests/test_manipulateSILVAtrees.py
+++ tests/test_manipulateSILVAtrees.py
@@ -1,7 +1,11 @@
 import unittest
+import matplotlib.cm as cm
 from Bio import AlignIO
 from ete3 import Tree
-from scripts.manipulateSILVAtrees import map_names_from_taxmap, read_taxonomy_map, connect_accessions_with_nucl, remove_nodes_from_tree
+from scripts.manipulateSILVAtrees import map_names_from_taxmap, read_taxonomy_map
+from scripts.manipulateSILVAtrees import connect_accessions_with_nucl, remove_nodes_from_tree
+from scripts.manipulateSILVAtrees import combine_tempura_with_tree, save_colored_tree_by_tempura
+from scripts.manipulateSILVAtrees import generate_nucleotide_type_set, save_shape_annotations
 
 class TestManipulateSILVAtrees(unittest.TestCase):
     
@@ -21,7 +25,8 @@ class TestManipulateSILVAtrees(unittest.TestCase):
         self.assertEqual(len(self.taxonomyMapper), 3)
 
     def test_connect_accessions_with_nucl(self):
-        accessionToNucl = connect_accessions_with_nucl(self.tree, self.align, self.nuclOne, self.nuclTwo, self.nameMapper, self.taxonomyMapper)
+        accessionToNucl = connect_accessions_with_nucl(self.tree, self.align, self.nuclOne, 
+            self.nuclTwo, self.nameMapper, self.taxonomyMapper)
         self.assertEqual(self.tree.children[0].nuclOfInterest[0], 'g')
         self.assertEqual(self.tree.children[1].nuclOfInterest[0], 'c')
         self.assertEqual(self.tree.children[2].nuclOfInterest[0], 'a')
@@ -32,13 +37,17 @@ class TestManipulateSILVAtrees(unittest.TestCase):
         self.nameMapper = map_names_from_taxmap('alns/taxmap_slv_lsu_parc_138.1.txt')
         self.taxonomyMapper = read_taxonomy_map('alns/tax_slv_lsu_138.1.tre')
         self.tree = Tree('alns/tax_slv_lsu_138.1.tre', format=1)
-        self.align = AlignIO.read('alns/PTC_2063_2447_2450_2452_2453_2500_2501_2504_fullArch.sto', "stockholm")
+        self.align = AlignIO.read('alns/PTC_2063_2447_2450_2452_2453_2500_2501_2504_fullArch.sto', 
+            "stockholm")
         self.nuclOne, self.nuclTwo = 0, 9
         self.assertEqual(len(self.tree.get_leaves()), 3630)
-        accessionToNucl = connect_accessions_with_nucl(self.tree, self.align, self.nuclOne, self.nuclTwo, self.nameMapper, self.taxonomyMapper)
+        accessionToNucl = connect_accessions_with_nucl(self.tree, self.align, self.nuclOne, 
+            self.nuclTwo, self.nameMapper, self.taxonomyMapper)
         self.assertEqual(len(self.tree.get_leaves()), 3630)
-        self.assertEqual(self.tree.children[0].children[3].children[2].nuclOfInterest[0], 'cgacaucu')
-        self.assertEqual(self.tree.children[0].children[3].children[2].name, 'Nitrososphaeria')
+        self.assertEqual(self.tree.children[0].children[3].children[2].nuclOfInterest[0], 
+            'cgacaucu')
+        self.assertEqual(self.tree.children[0].children[3].children[2].name, 
+            'Nitrososphaeria')
         namesForTruncation = ['uncultured', 'Rice Cluster I', 'J07HB67', 'J07HR59', 'J07HX64']
         truncatedTree, removed_Nodes = remove_nodes_from_tree(self.tree, namesForTruncation)
         self.assertEqual(len(truncatedTree.get_leaves()), 3558)
@@ -46,11 +55,101 @@ class TestManipulateSILVAtrees(unittest.TestCase):
 
     def test_node_deletion(self):
         self.tree = Tree('alns/tax_slv_lsu_138.1.tre', format=1)
-        self.align = AlignIO.read('alns/PTC_2063_2447_2450_2452_2453_2500_2501_2504_fullArch.sto', "stockholm")
+        self.align = AlignIO.read('alns/PTC_2063_2447_2450_2452_2453_2500_2501_2504_fullArch.sto', 
+            "stockholm")
         self.nuclOne, self.nuclTwo = 0, 9
         self.assertEqual(len(self.tree.get_leaves()), 3630)
-        accessionToNucl = connect_accessions_with_nucl(self.tree, self.align, self.nuclOne, self.nuclTwo, self.nameMapper, self.taxonomyMapper)
+        accessionToNucl = connect_accessions_with_nucl(self.tree, self.align, self.nuclOne, 
+            self.nuclTwo, self.nameMapper, self.taxonomyMapper)
         self.assertEqual(len(self.tree.get_leaves()), 2)
+    
+    def test_combine_tempura_with_tree(self):
+        self.nameMapper = map_names_from_taxmap('alns/taxmap_slv_lsu_parc_138.1.txt')
+        self.taxonomyMapper = read_taxonomy_map('alns/tax_slv_lsu_138.1.tre')
+        self.tree = Tree('alns/tax_slv_lsu_138.1.tre', format=1)
+        self.align = AlignIO.read('alns/PTC_2063_2447_2450_2452_2453_2500_2501_2504_fullArch.sto', 
+            "stockholm")
+        self.nuclOne, self.nuclTwo = 0, 9
+        accessionToNucl = connect_accessions_with_nucl(self.tree, self.align, self.nuclOne, 
+            self.nuclTwo, self.nameMapper, self.taxonomyMapper)
+        namesForTruncation = ['uncultured', 'Rice Cluster I', 'J07HB67', 'J07HR59', 'J07HX64']
+        truncatedTree, removed_Nodes = remove_nodes_from_tree(self.tree, namesForTruncation)
+        toptList, toptMatchList, tempLabeledLeaves = combine_tempura_with_tree('tests/test_data/TEMPURA_test.csv', 
+            truncatedTree)
+        self.assertEqual(len(toptList), 5)
+        self.assertEqual(len(toptMatchList), 3)
+        self.assertEqual(len(tempLabeledLeaves), 3)
+        self.assertEqual(toptList[0], 100.0)
+        self.assertEqual(toptMatchList[1], 105.0)
+        self.assertEqual(tempLabeledLeaves[0], 'Methanopyrus')
+        has_temp = [hasattr(x,'topt_ave') for x in truncatedTree.traverse() 
+            if x.name == tempLabeledLeaves[0]]
+        self.assertEqual(has_temp[0], True)
+        mapper = cm.ScalarMappable()
+        save_colored_tree_by_tempura('test', truncatedTree, toptList, mapper, 
+            location='./tests/test_data/')
+        with open('tests/test_data/temperatureLabelColors_test.txt', 'r') as f:
+            for i, line in enumerate(f):
+                if i == 0:
+                    self.assertEqual(line, 'DATASET_STYLE\n')
+                elif i == 1:
+                    self.assertEqual(line, 'SEPARATOR COMMA\n')
+                elif i == 2:
+                    self.assertEqual(line, 'DATASET_LABEL,Colors 30.0-106.0\n')
+                elif i == 3:
+                    self.assertEqual(line, 'COLOR,#ffff00\n')
+                elif i == 4:
+                    self.assertEqual(line, 'DATA\n')
+                elif i == 53:
+                    self.assertEqual(line.split(',')[0], 'Methanopyrus')
+                else:
+                    self.assertEqual(len(line.split(',')), 7)
+            self.assertEqual(i, 3562)
+
+    def test_save_shape_annotations(self):
+        self.nameMapper = map_names_from_taxmap('alns/taxmap_slv_lsu_parc_138.1.txt')
+        self.taxonomyMapper = read_taxonomy_map('alns/tax_slv_lsu_138.1.tre')
+        self.tree = Tree('alns/tax_slv_lsu_138.1.tre', format=1)
+        self.align = AlignIO.read('alns/PTC_2063_2447_2450_2452_2453_2500_2501_2504_fullArch.sto', 
+            "stockholm")
+        self.nuclOne, self.nuclTwo = 0, 2
+        accessionToNucl = connect_accessions_with_nucl(self.tree, self.align, self.nuclOne, 
+            self.nuclTwo, self.nameMapper, self.taxonomyMapper)
+        namesForTruncation = ['uncultured', 'Rice Cluster I', 'J07HB67', 'J07HR59', 'J07HX64']
+        truncatedTree, removed_Nodes = remove_nodes_from_tree(self.tree, namesForTruncation)
+        
+        setOfNuclTypes = generate_nucleotide_type_set(accessionToNucl)
+        self.assertEqual(len(setOfNuclTypes), 14)
+        shapes = ["1","2","3","4","5","6","HH","HV","EL","DI","PL","PR","PU","PD","OC","GP"]
+
+        truncatedTree = save_shape_annotations(shapes, setOfNuclTypes, accessionToNucl, 
+            'Test', truncatedTree, outpath='tests/test_data/')
+        with open(file='tests/test_data/interestNuclShapes_Test.txt', mode='r') as f:
+            for i, line in enumerate(f):
+                if i == 0:
+                    self.assertEqual(line, 'DATASET_SYMBOL\n')
+                elif i == 1:
+                    self.assertEqual(line, 'SEPARATOR COMMA\n')
+                elif i == 2:
+                    self.assertEqual(line, 'DATASET_LABEL,SILVA tree shapes\n')
+                elif i == 3:
+                    self.assertEqual(line, 'COLOR,#ff0000\n')
+                elif i == 4:
+                    self.assertEqual(line, 'LEGEND_TITLE,Nucleotide types,\n')
+                elif i == 5:
+                    self.assertEqual(line, 'LEGEND_SHAPES,1,2,3,4,5,6,HH,HV,EL,DI,PL,PR,PU,PD\n')
+                elif i == 6:
+                    self.assertEqual(line, 'LEGEND_COLORS,#000000,#000000,#000000,#000000,#000000,#000000,#000000,#000000,#000000,#000000,#000000,#000000,#000000,#000000            \n')
+                elif i == 7:
+                    self.assertEqual(line, 'LEGEND_LABELS,cg,ca,-g,cc,aa,c-,-u,ag,ua,u-,uu,-a,gg,XX\n')
+                elif i == 8:
+                    self.assertEqual(line, 'DATA\n')
+                elif i == 57:
+                    self.assertEqual(line.split(',')[0], 'Methanopyrus')
+                    self.assertEqual(line.split(',')[1], '1')
+                else:
+                    self.assertEqual(len(line.split(',')), 6)
+
 
 if __name__ == '__main__':
     unittest.main()
